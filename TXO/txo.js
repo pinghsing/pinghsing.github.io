372db9eb337e3c56cbf99f75260cd0bf3aeb13ee
pinghsing <b04502010@ntu.edu.tw>
2017-07-31 23:08:49 +0800

    txo

--- TXO/txo.js
+++ TXO/txo.js
@@ -3,11 +3,11 @@ function TXO() {
             var amount=Number(document.getElementById("amount").value);
             var contract=Number(document.getElementById("contract").value);
             var e = document.getElementById("cp");
-            var cp=e.options[e.selectedIndex].value;
+            var cp=Number(e.options[e.selectedIndex].value);
             var cp=Number(document.getElementById("strike").value);
             var cost=price*50*amount;
             var income=(strike-contract)*cp*50*amount;
             if(income<0)income=0;
             var revenue=income-cost;
-            document.getElementById("txo").innerText = revenue;
+            document.getElementById("txo").innerText=revenue;
         }
