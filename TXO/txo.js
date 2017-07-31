76e429a14436f6e10e50a850a9a5a58bfd78170d
pinghsing <b04502010@ntu.edu.tw>
2017-07-31 23:05:43 +0800

    txo

--- TXO/txo.js
+++ TXO/txo.js
@@ -1,6 +1,13 @@
-function eCPM() {
-            var ctr = Number(document.getElementById("ctr").value);
-            var cpc = Number(document.getElementById("cpc").value);
-            var ecpm = 10*ctr*cpc;
-            document.getElementById("ecpm").innerText = ecpm;
+function TXO() {
+            var price=Number(document.getElementById("price").value);
+            var amount=Number(document.getElementById("amount").value);
+            var contract=Number(document.getElementById("contract").value);
+            var e = document.getElementById("cp");
+            var cp=e.options[e.selectedIndex].value;
+            var cp=Number(document.getElementById("strike").value);
+            var cost=price*50*amount;
+            var income=(strike-contract)*cp*50*amount;
+            if(income<0)income=0;
+            var revenue=income-cost;
+            document.getElementById("txo").innerText = revenue;
         }
