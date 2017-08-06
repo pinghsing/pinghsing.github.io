0fc8ef78acf7764c80cb2a832fd0e1bfec6abb19
pinghsing <b04502010@ntu.edu.tw>
2017-08-06 22:52:26 +0800

    hw2

--- hw2/js/index.js
+++ hw2/js/index.js
@@ -20,14 +20,20 @@ $('#insert').on('click', function() {
     }, "json");
 })
 
+function showitem(value){
+    var name=value.name;
+    var price=Number(value.price);
+    var count=Number(value.count);
+    var image=value.image;
+}
+
 $('#query').on('click', function() {
     $.get("https://js2017-hw2.kchen.club/query", function(response) {
         if (response) {
             if (response.result) {
-
                 // TODO HW2 作業寫在這裡
-
-
+                var data=response.data;
+                data.forEach(showitem);
             } else {
                 $('#message').text('查詢失敗 (' + response.data + ')');
                 $('#dialog').modal('show');
